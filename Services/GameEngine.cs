0099c643236c129079d8a96e90b7cc12f28ac2b7
KarbonKitty <308lapua.magnum@gmail.com>
2023-03-06 20:07:36 +0100

    Better job change UI
    Display more information about the new job, including description and
    production.

--- Services/GameEngine.cs
+++ Services/GameEngine.cs
@@ -10,7 +10,8 @@ public class GameEngine : IGameEngine
 
     public CircularBuffer<string> Log { get; } = new(15);
 
-    public SimplePrice NewSheepPrice => SheepData.NewSheepBasePrice * Math.Pow(1.15, State.Sheep.Count);
+    public SimplePrice NewSheepPrice
+        => SheepData.NewSheepBasePrice * Math.Pow(1.15, State.Sheep.Count);
 
     private IEnumerable<GameObject> AllGameObjects
         => State.Hunts
